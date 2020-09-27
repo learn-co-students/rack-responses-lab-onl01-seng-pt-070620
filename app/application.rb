afaee00e1680ffd3cff7dfc44f5d5347711187d4
jmd <jordan.michael.dickey@gmail.com>
2020-09-26 20:34:38 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+require 'rack'
+ 
+class Application
+  def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, greeting ]
+  end
+ 
+  def greeting
+    t = Time.now
+    (t.hour >=  12) ?  ["<em>Good Afternoon!<em>"] : ["<em>Good Morning!<em>"]
+  end
+end
+ 
